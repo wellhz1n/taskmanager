06344b016dc68315b1ae9308939a50170f914877
Wellington Hellstrom Ramos <48489005+wellhz1n@users.noreply.github.com>
2024-04-02 11:52:13 +0000

    first commit

---
+++ app/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Card from "./components/card/Card.vue"
+</script>
+
+<template>
+  <div class="cards-container-list">
+    <Card v-for="i in 10" :title="'Teste'+i" message="Teste de mensagem card"/>
+  </div>
+</template>
+
+<style scoped>
+.cards-container-list{
+  display: flex;
+  width: 100%;
+  gap: 10px;
+  flex-wrap: wrap;
+}
+</style>
---
+++ app/src/components/card/Card.vue
@@ -0,0 +1,28 @@
+<script setup  lang="ts">
+
+defineProps<ICardProps>()
+
+interface ICardProps{
+    title:string,
+    message:string
+}
+</script>
+<template>
+<div class="cardComponentContainer">
+    <h3>{{title}}</h3>
+    <h3>{{message}}</h3>
+
+</div>
+</template>
+<style scoped>
+.cardComponentContainer{
+    border: 1px solid #444;
+    background: #ffffff;
+    padding: 16px;
+    border-radius: 12px;
+}
+
+.cardComponentContainer:hover{
+    filter: drop-shadow(0px  0px 16px rgba(30, 58, 26, 0.438));
+}
+</style>
